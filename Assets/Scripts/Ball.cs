0ddb498ff6ace93cddd825d900064c933780bf7b
Cam <cam+gh2@caesium.co.nz>
2023-02-26 22:01:00 +1300

    Reorganise and tweak.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
@@ -12,26 +10,12 @@ public class Ball : MonoBehaviour
         Reset();
     }
 
-    private void FixedUpdate()
-    {
-        var velocity = rigidbody.velocity;
-        if (velocity.magnitude <= 0.1f)
-        {
-            rigidbody.AddForce(new Vector2(0.1f, 0.1f));
-        }
-        else if (velocity.magnitude > 5.0f)
-        {
-
-        }
-    }
-
     private void Reset()
     {
         transform.position = new Vector3(0.0f, 0.0f, 0.0f);
         var speedY = Random.Range(-3.0f, 3.0f);
 
         rigidbody.velocity = new Vector2(-10.0f, speedY);
-        // rigidbody.angularVelocity = 0.0f;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
