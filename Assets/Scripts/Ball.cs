ca54a72c9b22df428885f87f19e4b393f03f93d8
Cam <cam+gh2@caesium.co.nz>
2023-02-27 23:29:56 +1300

    Make ball start faster.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,21 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
     private new Rigidbody2D rigidbody;
+    private List<Vector2> startVectors;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        startVectors = new List<Vector2>();
+        // Setup 4 diagonal Vectors for initial ball direction
+        startVectors.Add(new Vector2(-10f, -10f));
+        startVectors.Add(new Vector2(-10f, 10f));
+        startVectors.Add(new Vector2(10f, -10f));
+        startVectors.Add(new Vector2(10f, 10f));
         Reset();
     }
 
     private void Reset()
     {
         transform.position = new Vector3(0.0f, 0.0f, 0.0f);
-        var speedY = Random.Range(-3.0f, 3.0f);
 
-        rigidbody.velocity = new Vector2(-10.0f, speedY);
+        var random = new System.Random();
+        rigidbody.velocity = startVectors[random.Next(startVectors.Count)];
     }
 
     private void OnTriggerEnter2D(Collider2D other)
